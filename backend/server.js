5aef4b063cf0f997a1e57fc3516349b56133137a
Alex Miserandino <amiserandino20@gmail.com>
2023-10-11 22:03:26 -0700

    updated middleware

--- backend/server.js
+++ backend/server.js
@@ -1,39 +1,38 @@
 "use strict";
 
 const express = require("express");
-const bodyParser = require("body-parser");
-const expect = require("chai").expect;
 const cors = require("cors");
-require("dotenv").config();
-
+const path = require("path");
 const apiRoutes = require("./routes/api.js");
-// const fccTestingRoutes = require("./routes/fcctesting.js");
-// const runner = require("./test-runner");
-const db = require("./dbconnect.js");
-let app = express();
+const app = express();
+//const fccTestingRoutes = require("./routes/fcctesting.js");
+//const runner = require("./test-runner");
+//require("dotenv").config();
+//const bodyParser = require("body-parser");
+//const expect = require("chai").expect;
 
-app.use("/public", express.static(process.cwd() + "/public"));
-
+app.use(express.static(path.join(__dirname, "../frontend/public")));
 app.use(cors({ origin: "*" })); //For FCC testing purposes only
-
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
+//app.use(bodyParser.urlencoded({ extended: true }));
 
-//Sample front-end
+//front-end
 app.route("/:project/").get(function (req, res) {
-  res.sendFile(process.cwd() + "/views/issue.html");
+  res.sendFile("issue.html", {
+    root: path.join(__dirname, "../frontend/views"),
+  });
 });
 
-//Index page (static HTML)
+//Index page
 app.route("/").get(function (req, res) {
-  res.sendFile(process.cwd() + "/views/index.html");
+  res.sendFile("index.html", {
+    root: path.join(__dirname, "../frontend/views"),
+  });
 });
 
-//For FCC testing purposes
-// fccTestingRoutes(app);
-
-// //Routing for API
-// apiRoutes(app);
+//Routing for API
+apiRoutes(app);
 
 //404 Not Found Middleware
 app.use(function (req, res, next) {
@@ -43,17 +42,17 @@ app.use(function (req, res, next) {
 //Start our server and tests!
 const listener = app.listen(process.env.PORT || 3000, function () {
   console.log("Your app is listening on port " + listener.address().port);
-  if (process.env.NODE_ENV === "test") {
-    console.log("Running Tests...");
-    setTimeout(function () {
-      try {
-        runner.run();
-      } catch (e) {
-        console.log("Tests are not valid:");
-        console.error(e);
-      }
-    }, 3500);
-  }
+  // if (process.env.NODE_ENV === "test") {
+  //   console.log("Running Tests...");
+  //   setTimeout(function () {
+  //     try {
+  //       runner.run();
+  //     } catch (e) {
+  //       console.log("Tests are not valid:");
+  //       console.error(e);
+  //     }
+  //   }, 3500);
+  // }
 });
 
 module.exports = app; //for testing
