512b4a678453c300d1143982ab3f494ff5b7ba81
Alex Miserandino <amiserandino20@gmail.com>
2023-10-28 23:57:07 -0700

    added useEffect

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,9 +2,9 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { terminal } from "virtual:terminal";
+import IssueList from "../components/IssueList";
 
 const Home = () => {
-  //state for issue
   const [issue, setIssue] = useState({
     issue_title: "",
     issue_text: "",
@@ -13,15 +13,29 @@ const Home = () => {
     status_text: "",
   });
 
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    const getIssues = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000");
+        setList(response.data);
+      } catch (err) {
+        terminal.log(err);
+      }
+    };
+    getIssues();
+  }, [issue]);
+
+terminal.log(list)
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setIssue({ ...issue, [name]: value });
-    terminal.log(issue);
   };
 
   const handleSubmit = async (event) => {
+    terminal.log(list, "list")
     event.preventDefault();
-
     if (
       issue.issue_title == "" ||
       issue.issue_text == "" ||
@@ -30,9 +44,11 @@ const Home = () => {
       alert("required fields missing!");
     } else {
       try {
-        await axios.post("http://localhost:3000/api/issues/:project", issue);
+        await axios.post("http://localhost:3000", issue).then((res) => {
+          //terminal.log(res.data);
+        });
       } catch (error) {
-        console.error(error);
+        terminal.error(error);
       }
       setIssue({
         issue_title: "",
@@ -45,61 +61,65 @@ const Home = () => {
   };
 
   return (
-    <div id="submitNewIssue">
-      <br />
-      <h3>Submit a new issue:</h3>
-      <form id="newIssue" method="post" action="/api/">
-        <input
-          type="text"
-          name="issue_title"
-          placeholder="*Title"
-          value={issue.issue_title}
-          style={{ width: "320px", marginBottom: "3px" }}
-          required={true}
-          onChange={handleInputChange}
-        />
+    <div>
+      <div id="submitNewIssue">
         <br />
-        <textarea
-          type="text"
-          name="issue_text"
-          value={issue.issue_text}
-          placeholder="*Text"
-          style={{ width: "320px", height: "100px", marginBottom: "-5px" }}
-          required={true}
-          onChange={handleInputChange}
-        ></textarea>
-        <br></br>
-        <input
-          type="text"
-          name="created_by"
-          placeholder="*Created by"
-          value={issue.created_by}
-          style={{ width: "100px", marginRight: "2px" }}
-          required={true}
-          onChange={handleInputChange}
-        ></input>
-        <input
-          type="text"
-          name="assigned_to"
-          value={issue.assigned_to}
-          placeholder="(opt)Assigned to"
-          style={{ width: "100px", marginRight: "2px" }}
-          onChange={handleInputChange}
-        ></input>
-        <input
-          type="text"
-          name="status_text"
-          value={issue.status_text}
-          placeholder="(opt)Status text"
-          style={{ width: "100px" }}
-          onChange={handleInputChange}
-        ></input>
-        <br />
-        <button type="submit" onClick={handleSubmit}>
-          Submit Issue
-        </button>
-      </form>
-      <div id="issueDisplay"></div>
+        <h3>Submit a new issue:</h3>
+        <form id="newIssue" method="post" action="/api/">
+          <input
+            type="text"
+            name="issue_title"
+            placeholder="*Title"
+            value={issue.issue_title}
+            style={{ width: "320px", marginBottom: "3px" }}
+            required={true}
+            onChange={handleInputChange}
+          />
+          <br />
+          <textarea
+            type="text"
+            name="issue_text"
+            value={issue.issue_text}
+            placeholder="*Text"
+            style={{ width: "320px", height: "100px", marginBottom: "-5px" }}
+            required={true}
+            onChange={handleInputChange}
+          ></textarea>
+          <br></br>
+          <input
+            type="text"
+            name="created_by"
+            placeholder="*Created by"
+            value={issue.created_by}
+            style={{ width: "100px", marginRight: "2px" }}
+            required={true}
+            onChange={handleInputChange}
+          ></input>
+          <input
+            type="text"
+            name="assigned_to"
+            value={issue.assigned_to}
+            placeholder="(opt)Assigned to"
+            style={{ width: "100px", marginRight: "2px" }}
+            onChange={handleInputChange}
+          ></input>
+          <input
+            type="text"
+            name="status_text"
+            value={issue.status_text}
+            placeholder="(opt)Status text"
+            style={{ width: "100px" }}
+            onChange={handleInputChange}
+          ></input>
+          <br />
+          <button type="submit" onClick={handleSubmit}>
+            Submit Issue
+          </button>
+        </form>
+      </div>
+      <div>
+        <IssueList list={list}/>
+      </div>
     </div>
   );
 };
