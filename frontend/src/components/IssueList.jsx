243f7c1e98c1f363e73e9e371d54949a2a9308bb
Alex Miserandino <amiserandino20@gmail.com>
2023-11-06 22:04:44 -0700

    added onClick for edit button

--- frontend/src/components/IssueList.jsx
+++ frontend/src/components/IssueList.jsx
@@ -1,35 +1,45 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { terminal } from "virtual:terminal";
-
+import { useNavigate } from "react-router-dom";
 
 const IssueList = ({ list }) => {
-
+  const navigate = useNavigate();
+  
+  //state for id
+  const [issueId, setIssueId] = useState("");
 
   const handleDeleteIssue = async (e) => {
     e.preventDefault();
     try {
-      await axios
-        .delete(`http://localhost:3000/${e.target.value}`)
+      await axios.delete(`http://localhost:3000/${e.target.value}`);
     } catch (error) {
       terminal.error(error);
     }
   };
-
 
   const handleCloseIssue = async (e) => {
     try {
-      await axios
-        .put("http://localhost:3000", { _id: e.target.value, open: false })
+      await axios.put("http://localhost:3000", {
+        _id: e.target.value,
+        open: false,
+      });
     } catch (error) {
       terminal.error(error);
     }
   };
+
+  useEffect(() => {
+    terminal.log(issueId);
+  }, [issueId]);
 
   return (
     <div>
       {list.map((item, key) => (
-        <div key={item._id} className={item.open ? "open-issue" : "closed-issue"}>
+        <div
+          key={item._id}
+          className={item.open ? "open-issue" : "closed-issue"}
+        >
           <p>id: {item._id}</p>
           <h3>
             {item.issue_title} - {item.open == true ? "(Open)" : "(Closed)"}
@@ -40,7 +50,14 @@ const IssueList = ({ list }) => {
           <p>Created on: {item.created_on}</p>
           <p>Updated on: {item.updated_on}</p>
           <div className="button-container">
-            <button>Edit</button>
+            <button
+              value={item._id}
+              onClick={(e) => {
+                navigate("/update", { state: item });
+              }}
+            >
+              Edit
+            </button>
             <button value={item._id} onClick={(e) => handleCloseIssue(e)}>
               Close
             </button>
