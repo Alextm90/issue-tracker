62e40cf9197ed90c13f6b73c7a4aa6844ffab2c0
Alex Miserandino <amiserandino20@gmail.com>
2023-11-22 23:14:58 -0700

    deleted files

--- backend/routes/users.js
+++
@@ -1,8 +0,0 @@
-const db = require("../Config/dbconnect.js");
-const express = require("express");
-const router = express.Router();
-const registerUser = require("../Controllers/userController.js")
-
-router.post("/signup", registerUser);
-
-module.exports = router;
