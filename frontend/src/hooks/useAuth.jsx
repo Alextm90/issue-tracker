a6c5e742349749d36154f78aa035b59882c01e0a
Alex Miserandino <amiserandino20@gmail.com>
2023-11-25 23:22:23 -0700

    Initial commit

---
+++ frontend/src/hooks/useAuth.jsx
@@ -0,0 +1,11 @@
+import { useContext } from "react";
+import AuthContext from "../utils/AuthProvider";
+
+const useAuth = () => {
+  return useContext(AuthContext);
+};
+
+export default useAuth;
+
+
+
