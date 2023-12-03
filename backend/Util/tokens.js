972b2e47b08479e2f7e5c9072bb5f9df88bdbf72
Alex Miserandino <amiserandino20@gmail.com>
2023-12-02 23:17:10 -0700

    add functions to send tokens

--- backend/Util/tokens.js
+++ backend/Util/tokens.js
@@ -2,13 +2,34 @@ require("dotenv").config();
 const jwt = require("jsonwebtoken");
 
 const createAccessToken = (id) => {
+  console.log('created')
   return jwt.sign({ id }, process.env.JWT_ACCESS_SECRET, {
-    expiresIn: '15m',
+    expiresIn: '25s',
   });
 };
 
 const createRefreshToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_REFRESH_SECRET);
+  return jwt.sign({ id }, process.env.JWT_REFRESH_SECRET, {
+    expiresIn: '7d',
+  });
 };
 
-module.exports = { createAccessToken, createRefreshToken };
+const sendAccessToken = (res, req, accesstoken, message) => {
+  console.log("sent")
+  res.json({
+    message,
+    accesstoken,
+    success: true,
+  });
+};
+
+const sendRefreshToken = (res, token) => {
+  res.cookie("refreshtoken", token, {
+    withCredentials: true,
+    httpOnly: true,
+    sameSite: "None",
+    secure: "true",
+  });
+};
+
+module.exports = { createAccessToken, createRefreshToken, sendAccessToken, sendRefreshToken };
