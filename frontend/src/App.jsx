14dfe2535bdb4e3459b0553f3f0bd2af22832732
Alex Miserandino <amiserandino20@gmail.com>
2023-12-02 23:22:27 -0700

    added pages/auth

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,30 @@
-import { useState } from "react";
-import "./App.css";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Update from "./pages/Update";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Navigation from "./components/navigation.jsx";
+import { AuthProvider } from "./utils/AuthProvider.jsx";
+import RequireAuth from "./components/RequireAuth.jsx";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
+    <AuthProvider>
       <Router>
+        <Navigation />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/update" element={<Update />} />
+          <Route element={<RequireAuth />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/update" element={<Update />} />
+          </Route>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
         </Routes>
       </Router>
+    </AuthProvider>
   );
-}
+};
 
 export default App;
