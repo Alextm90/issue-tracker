16d7535b62a8dd5401ed8dc75d5b7291f2c9f0f5
Alex Miserandino <amiserandino20@gmail.com>
2023-12-26 22:22:08 -0700

    refactored

--- backend/Middleware/verifyToken.js
+++ backend/Middleware/verifyToken.js
@@ -1,15 +1,19 @@
 const jwt = require("jsonwebtoken");
 
 const verifyToken = (req, res, next) => {
-  const header = req.headers["authorization"];
+  const header = req.headers.authorization || req.headers.Authorization;
+  console.log(header, "header");
   const accessToken = header && header.split(" ")[1];
-  console.log(accessToken, "accesTokenn")
-  if (accessToken == null) return res.sendStatus(401)
+  console.log(accessToken, "accesTokenn");
+  if (accessToken == null) {
+  console.log("now")
+  return res.sendStatus(401);
+  } 
   jwt.verify(accessToken, process.env.JWT_ACCESS_SECRET, (err, user) => {
-    if (err) return res.sendStatus(403)
-    req.user = user
+    if (err) return res.status(403);
+    req.user = user;
   });
-  next()
+  next();
 };
 
-module.exports = verifyToken
+module.exports = verifyToken;
