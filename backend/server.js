b29abddb6cd9289b63482991c648186f3725c44e
Alex Miserandino <amiserandino20@gmail.com>
2024-01-11 22:29:08 -0700

    updated cors

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,24 @@
 "use strict";
 const express = require("express");
 const cors = require("cors");
-const path = require("path");
-const issue = require("./Routes/issues.js");
-const user = require("./Routes/users.js");
+const issue = require("./Routes/issueRoutes.js");
+const user = require("./Routes/authRoutes.js");
 const app = express();
 require("dotenv").config();
 const cookieParser = require("cookie-parser");
-//const expect = require("chai").expect;
 
 app.use(
   cors({
-    origin: ["http://localhost:3001"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    origin: true,
     credentials: true,
   })
 );
+
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-
+//Routes
 app.use("/", issue);
 app.use("/", user)
 
@@ -32,17 +30,18 @@ app.use(function (req, res, next) {
 //Start our server and tests!
 const listener = app.listen(process.env.PORT || 3000, function () {
   console.log("Your app is listening on port " + listener.address().port);
-  // if (process.env.NODE_ENV === "test") {
-  //   console.log("Running Tests...");
-  //   setTimeout(function () {
-  //     try {
-  //       runner.run();
-  //     } catch (e) {
-  //       console.log("Tests are not valid:");
-  //       console.error(e);
-  //     }
-  //   }, 3500);
-  // }
+//   if (process.env.NODE_ENV === "test") {
+//     console.log("Running Tests...");
+//     setTimeout(function () {
+//       try {
+//         runner.run();
+//       } catch (e) {
+//         console.log("Tests are not valid:");
+//         console.error(e);
+//       }
+//     }, 3500);
+//   }
 });
 
 module.exports = app; //for testing
+
