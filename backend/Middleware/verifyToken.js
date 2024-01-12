212489e48d24ee9fd788723bd2ecccd597b2b682
Alex Miserandino <amiserandino20@gmail.com>
2024-01-11 22:23:12 -0700

    updated jwt.verify

--- backend/Middleware/verifyToken.js
+++ backend/Middleware/verifyToken.js
@@ -2,7 +2,6 @@ const jwt = require("jsonwebtoken");
 
 const verifyToken = (req, res, next) => {
   const header = req.headers.authorization || req.headers.Authorization;
-  console.log(header, "header");
   const accessToken = header && header.split(" ")[1];
   console.log(accessToken, "accesTokenn");
   if (accessToken == null) {
@@ -10,10 +9,13 @@ const verifyToken = (req, res, next) => {
   return res.sendStatus(401);
   } 
   jwt.verify(accessToken, process.env.JWT_ACCESS_SECRET, (err, user) => {
-    if (err) return res.status(403);
+    if (err) {
+    console.log("error here")
+    return res.status(403).json({ message: "Forbidden" });
+    }
     req.user = user;
+    next();
   });
-  next();
 };
 
 module.exports = verifyToken;
