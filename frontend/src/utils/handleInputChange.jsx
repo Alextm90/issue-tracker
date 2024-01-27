4781f014ef7056a18573c01b3558a2f45996e207
Alex Miserandino <amiserandino20@gmail.com>
2024-01-26 21:18:20 -0700

    removed terminal.log

--- frontend/src/utils/handleInputChange.jsx
+++ frontend/src/utils/handleInputChange.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { terminal } from "virtual:terminal";
 
  const handleInputChange = (e, setIssue, issue) => {
-    terminal.log(issue)
     const { name, value } = e.target;
     setIssue({ ...issue, [name]: value });
   };
