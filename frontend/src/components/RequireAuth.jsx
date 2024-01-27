af6c35cc54f348e1bd6ffadaf3833258f80a1c30
Alex Miserandino <amiserandino20@gmail.com>
2024-01-26 21:17:31 -0700

    removed terminal.log

--- frontend/src/components/RequireAuth.jsx
+++ frontend/src/components/RequireAuth.jsx
@@ -5,11 +5,11 @@ import { terminal } from "virtual:terminal";
 const RequireAuth = () => {
   const { auth } = useAuth();
 
-  terminal.log(auth, "auth")
-  
   if (auth !== null) {
     return auth ? <Outlet /> : <Navigate to="/login" />;
   }
 };
 
 export default RequireAuth;
+
+
