221c91f84469af836ececb49411049c6d8ab2675
Alex Miserandino <amiserandino20@gmail.com>
2024-02-04 20:42:53 -0700

    removed comments

--- backend/Models/issue.js
+++ backend/Models/issue.js
@@ -2,12 +2,12 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-  issue_title: { type: String }, //might need to change back to required
-  issue_text: { type: String }, //might need to change back to required
+  issue_title: { type: String }, 
+  issue_text: { type: String }, 
   created_on: { type: Date, required: true },
   updated_on: { type: Date, required: true },
   open: { type: Boolean, required: true },
-  created_by: { type: String }, //might need to change back to required
+  created_by: { type: String }, 
   assigned_to: String,
   status_text: String,
   project: String,
