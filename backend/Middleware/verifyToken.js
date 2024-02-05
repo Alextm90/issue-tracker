a4604f8eb561b6589fb17e53b5378ce92e7223b9
Alex Miserandino <amiserandino20@gmail.com>
2024-02-04 20:40:16 -0700

    removed console.log

--- backend/Middleware/verifyToken.js
+++ backend/Middleware/verifyToken.js
@@ -3,14 +3,11 @@ const jwt = require("jsonwebtoken");
 const verifyToken = (req, res, next) => {
   const header = req.headers.authorization || req.headers.Authorization;
   const accessToken = header && header.split(" ")[1];
-  console.log(accessToken, "accesTokenn");
   if (accessToken == null) {
-  console.log("now")
   return res.sendStatus(401);
   } 
   jwt.verify(accessToken, process.env.JWT_ACCESS_SECRET, (err, user) => {
     if (err) {
-    console.log("error here")
     return res.status(403).json({ message: "Forbidden" });
     }
     req.user = user;
