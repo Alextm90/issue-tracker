04256753ea23759ce1f79bac899aaf6264d5a5b6
Alex Miserandino <amiserandino20@gmail.com>
2024-02-05 14:05:22 -0700

    updated baseURL

--- frontend/src/api/axiosInstance.jsx
+++ frontend/src/api/axiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://issue-tracker-nwp9.onrender.com",
   withCredentials: true,
   headers: { "Content-Type": "application/json" },
 });
