5b88c81ceef2dd9678e00abb5cb81b519a435d5a
Alex Miserandino <amiserandino20@gmail.com>
2024-08-05 21:04:14 -0700

    replaced bcrypt w/ bcryptjs

--- backend/Controllers/authController.js
+++ backend/Controllers/authController.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 const User = require("../Models/user.js");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const ObjectId = require("mongoose").Types.ObjectId;
 const {
   createAccessToken,
