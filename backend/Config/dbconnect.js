b85bc5c7eddc49bd981bb3fe2f4383b8dad3916e
Alex Miserandino <amiserandino20@gmail.com>
2024-08-06 09:15:16 -0700

    added error handling

--- backend/Config/dbconnect.js
+++ backend/Config/dbconnect.js
@@ -1,9 +1,19 @@
-mongoose = require("mongoose");
+const mongoose = require("mongoose");
 require("dotenv").config();
 
-const db = mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+const connectToDatabase = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected successfully.");
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+    process.exit(1);
+  }
+};
+
+const db = connectToDatabase();
 
 module.exports = db;
