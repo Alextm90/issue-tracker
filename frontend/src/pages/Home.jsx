74b010618650be8531cd668faf9937b689195f43
Alex Miserandino <amiserandino20@gmail.com>
2024-08-27 22:33:57 -0700

    updated useState and issue fields

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,21 +1,20 @@
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import { terminal } from "virtual:terminal";
-import IssueList from "../components/IssueList";
-import Form from "../components/Form.jsx";
-import handleInputChange from "../utils/handleInputChange";
-import useAxiosInstance from "../hooks/useAxiosInstance";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { terminal } from 'virtual:terminal';
+import IssueList from '../components/IssueList';
+import Form from '../components/Form.jsx';
+import handleInputChange from '../utils/handleInputChange';
+import useAxiosInstance from '../hooks/useAxiosInstance';
 
 const Home = () => {
   const axiosInstance = useAxiosInstance();
   const [issue, setIssue] = useState({
-    issue_title: "",
-    issue_text: "",
-    created_by: "",
-    assigned_to: "",
-    status_text: "",
+    issue_title: '',
+    issue_text: '',
+    created_by: '',
+    assigned_to: '',
   });
 
   const [list, setList] = useState([]);
@@ -27,7 +26,7 @@ const Home = () => {
   const getIssues = async () => {
     try {
       const response = await axios.get(
-        "https://my-backend-latest-0ftm.onrender.com"
+        'https://my-backend-latest-0ftm.onrender.com',
       );
       setList(response.data);
     } catch (err) {
@@ -38,27 +37,26 @@ const Home = () => {
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      const response = await axiosInstance.post("/", issue);
+      const response = await axiosInstance.post('/', issue);
       const { success } = response.data;
       getIssues();
     } catch (error) {
-      toast("Please enter required fields!", {
-        position: "top-right",
+      toast('Please enter required fields!', {
+        position: 'top-right',
         autoClose: 5000,
         hideProgressBar: false,
         closeOnClick: true,
         pauseOnHover: true,
         draggable: true,
         progress: undefined,
-        theme: "light",
+        theme: 'light',
       });
     }
     setIssue({
-      issue_title: "",
-      issue_text: "",
-      created_by: "",
-      assigned_to: "",
-      status_text: "",
+      issue_title: '',
+      issue_text: '',
+      created_by: '',
+      assigned_to: '',
     });
   };
 
@@ -66,7 +64,7 @@ const Home = () => {
     <div>
       <div id="submitNewIssue">
         <br />
-        <h3>Submit a new issue:</h3>
+        <h3 id='issue-header'>Submit a new issue:</h3>
         <Form
           issue={issue}
           handleInputChange={(e) => handleInputChange(e, setIssue, issue)}
