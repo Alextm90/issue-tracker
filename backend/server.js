90d1ee1de34562de0bd53cee3a5bf79feff26e83
Alex Miserandino <amiserandino20@gmail.com>
2024-09-06 16:56:32 -0400

    removed comments

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,17 @@
-"use strict";
-const express = require("express");
-const cors = require("cors");
-const issue = require("./routes/issueRoutes.js");
-const user = require("./routes/authRoutes.js");
+'use strict';
+const express = require('express');
+const cors = require('cors');
+const issue = require('./routes/issueRoutes.js');
+const user = require('./routes/authRoutes.js');
 const app = express();
-require("dotenv").config();
-const cookieParser = require("cookie-parser");
+require('dotenv').config();
+const cookieParser = require('cookie-parser');
 
 app.use(
   cors({
     origin: true,
     credentials: true,
-  })
+  }),
 );
 
 app.use(cookieParser());
@@ -19,29 +19,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 //Routes
-app.use("/", issue);
-app.use("/", user)
+app.use('/', issue);
+app.use('/', user);
 
 //404 Not Found Middleware
 app.use(function (req, res, next) {
-  res.status(404).type("text").send("Not Found");
+  res.status(404).type('text').send('Not Found');
 });
 
-//Start our server and tests!
-const listener = app.listen(10000, "0.0.0.0", function () {
-  console.log("Your app is listening on port " + listener.address().port);
-  //   if (process.env.NODE_ENV === "test") {
-  //     console.log("Running Tests...");
-  //     setTimeout(function () {
-  //       try {
-  //         runner.run();
-  //       } catch (e) {
-  //         console.log("Tests are not valid:");
-  //         console.error(e);
-  //       }
-  //     }, 3500);
-  //   }
+const listener = app.listen(10000, '0.0.0.0', function () {
+  console.log('Your app is listening on port ' + listener.address().port);
 });
 
 module.exports = app; //for testing
-
